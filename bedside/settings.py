4a42c5474dc0844cb27ce17d3ea383ea49b455e6
BlessingUhu <Blessing2002.feb@gmail.com>
2023-05-21 00:09:14 -0400

    installed django rest_framework and also added the requrements.txt for python installations

--- bedside/settings.py
+++ bedside/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 INSTALLED_APPS = [
     'main',
+    'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
